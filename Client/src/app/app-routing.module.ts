c97d6db2fdb959364eee9dffe4f15eaf1bfefa5c
Snooking <adriankon123@wp.pl>
2019-05-12 23:39:48 +0200

    Introduced routing

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharacterComponent } from './Character/character/character.component'
+import { QuestsComponent } from './Quests/quests/quests.component'
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: '', component: CharacterComponent },
+  { path: 'character', component: CharacterComponent },
+  { path: 'quests', component: QuestsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
